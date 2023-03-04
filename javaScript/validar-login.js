a47d7c4cf739010a86c650737b39e425b05cf6b1
jhonrichard97 <jhonrichardurbano@gmail.com>
2023-03-04 15:28:47 -0500

    update

---
+++ javaScript/validar-login.js
@@ -0,0 +1,30 @@
+function validar() {
+    let usuario = document.getElementById('inputEmail').value;
+    let contrasena = document.getElementById('inputPassword').value;
+
+    //validar correo
+    var ExpRegUsuario = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if (usuario == null || usuario.length == 0) {
+        document.getElementById('mensajeEmail').innerHTML = "Ingresar correo (obligatorio)";
+        document.getElementById('mensajeEmail').style.color = "red";
+        location.href = "#inputEmail";
+        return false;
+    } else if (usuario.match(ExpRegUsuario) == null) {
+        document.getElementById('mensajeEmail').innerHTML = "Ingresar correo valido";
+        document.getElementById('mensajeEmail').style.color = "red";
+        location.href = "#inputEmail";
+        return false;
+    } else {
+        document.getElementById('mensajeEmail').innerHTML = "";
+    }
+
+    //validar contraseña
+    if (contrasena == null || contrasena.length == 0) {
+        document.getElementById('mensajeContrasena').innerHTML = "Ingresar contraseña (obligatorio)";
+        document.getElementById('mensajeContrasena').style.color = "red";
+        location.href = "#inputPassword";
+        return false;
+    } else {
+        document.getElementById('mensajeContrasena').innerHTML = "";
+    }
+}
